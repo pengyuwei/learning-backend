4e9df2395ccfad4aa4325fff2a14b7df54788cf7
0xff <pengyuwei@gmail.com>
2021-10-27 15:59:01 +0800

    两种排序方法独立成两个文件，看得更清楚

--- golang/base/sort1.go
+++ golang/base/sort1.go
@@ -7,21 +7,6 @@ import (
 
 type Data [][]string
 
-func (p Data) Len() int {
-    return len(p)
-}
-
-func (p Data) Less(i, j int) bool {
-    // 以第五个元素为排序依据
-    // 正序：<
-    // 倒序：>
-    return p[i][5] < p[j][5]
-}
-
-func (p Data) Swap(i, j int) {
-    p[i], p[j] = p[j], p[i]
-}
-
 func main() {
     var d Data
     d = [][]string{
@@ -32,7 +17,15 @@ func main() {
         {"e", "e1", "e2", "e3", "e4", "1"},
     }
 
-    // 依赖Len、Less、Swap三个方法的实现
-    sort.Sort(d)
+    // Slice方法不需要Len、Less、Swap三个方法
+    sort.Slice(d, func(i, j int) bool {
+        // 正序：>
+        return d[i][5] < d[j][5]
+    })
     fmt.Println("正序：", d)
+    sort.Slice(d, func(i, j int) bool {
+        // 倒序：>
+        return d[i][5] > d[j][5]
+    })
+    fmt.Println("倒序：", d)
 }
