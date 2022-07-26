95bbf4ed23860ca59bbbf8d3231fc168980c36eb
0xff <pengyuwei@gmail.com>
2022-07-26 09:54:45 +0800

    gameloop

--- rust/trpl/guessing_game/src/main.rs
+++ rust/trpl/guessing_game/src/main.rs
@@ -8,22 +8,27 @@ fn main() {
     let secret_num = rand::thread_rng().gen_range(1, 101); // [1, 100]
     println!("secret_num is: {}", secret_num);
     
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse() // overwrite string guess var
-        .expect("Please type a number!"); 
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Please input your guess.");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = guess.trim().parse() // overwrite string guess var
+            .expect("Please type a number!"); 
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
